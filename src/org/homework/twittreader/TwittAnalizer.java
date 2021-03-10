670b7d8eaad2bd5140dbd69497bcf6482b06b2f3
zamdirit <kimp07@gmail.com>
2021-03-10 16:11:39 +0200

    string length arithmetic...

--- src/org/homework/twittreader/TwittAnalizer.java
+++ src/org/homework/twittreader/TwittAnalizer.java
@@ -2,52 +2,55 @@ package org.homework.twittreader;
 
 public class TwittAnalizer {
 
-    private static char space = ' ';
+    private static final char SPACE_CHAR = ' ';
+    private static final char TIME_DELIMITER = ':';
+
+    private TwittAnalizer() {}
 
     private static String normilizeBody(String body) {
-        String result = "";
+        StringBuilder result = new StringBuilder("");
         if (body.length() == 0) {
-            return result;
+            return "";
         }
         for (int i = 0; i < body.length(); i++) {
             char substr = body.charAt(i);
             if ((substr >= 'a' && substr <= 'z') || (substr >= 'A' && substr <= 'Z') || (substr >= '0' && substr <= '9') || substr == '\'' || substr == '`') {
-                result += substr;
+                result.append(substr);
             } else {
-                result += " ";
+                result.append(SPACE_CHAR);
             }
         }
-        return result.trim();
+        return result.toString().trim();
     }
 
-    private static float analizeSantiment(String body, Vocabulary vocabulary, int pos, int endPos, Twitt twitt) {
-        float weight = 0;
-        String santiment = body.substring(pos, endPos - 1).trim();
+    private static int analizeSantimentAndReturnFoundedSantimentLength(String body, Vocabulary vocabulary, int pos, int endPos, Twitt twitt) {
+        int santimentLength = 0;
+        String santiment = body.substring(pos, endPos).trim();
         while (santiment.length() >= vocabulary.getMinSantimentLength()) {
             float santimentWeight = vocabulary.getSantimentWeight(santiment);
             if (santimentWeight != 0.0f) {
-                weight = santimentWeight;
+                santimentLength = santiment.length();
                 twitt.appentFoundedSantiment(santiment);
+                twitt.addWeight(santimentWeight);
                 break;
             }
-            int lastSpace = santiment.lastIndexOf(space);
+            int lastSpace = santiment.lastIndexOf(SPACE_CHAR);
             if (lastSpace > 0) {
                 santiment = santiment.substring(0, lastSpace).trim(); // last symbol before space
             } else {
                 santiment = "";
             }
         }
-        return weight;
+        return santimentLength;
     }
 
     public static void analizeTwitt(Twitt twitt, Vocabulary vocabulary) {
 
-        double weight = 0;
         String body = twitt.getBody();
 
-        int pos = body.indexOf(':');
+        int pos = body.indexOf(TIME_DELIMITER);
         if (pos >= 0) { // throw 2nd ':' - twitt time
-            pos = body.indexOf(":", pos + 1);
+            pos = body.indexOf(TIME_DELIMITER, pos + 1);
             if (pos > 0) {
                 pos += 3;
             }
@@ -61,17 +64,15 @@ public class TwittAnalizer {
             System.out.println(body);
         }
         pos = 0;
-        int bodySize = body.length() - 1;
+        int bodySize = body.length();
         while (pos <= (bodySize - vocabulary.getMinSantimentLength()) && pos >= 0) {
-            if (body.charAt(pos) == space) {
+            if (body.charAt(pos) == SPACE_CHAR) {
                 pos++;
             } else {
                 int endPos = Math.min(pos + vocabulary.getMaxSantimentLength(), bodySize);
-                weight += analizeSantiment(body, vocabulary, pos, endPos, twitt);
-                pos = body.indexOf(space, pos);
+                pos += analizeSantimentAndReturnFoundedSantimentLength(body, vocabulary, pos, endPos, twitt);
+                pos = body.indexOf(SPACE_CHAR, pos);
             }
-
         }
-        twitt.setWeight(weight);
     }
 }
